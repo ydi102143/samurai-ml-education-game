4ceb7c4afa627cc483cfe8a077b41d0206346d11
Your Name <you@example.com>
2025-10-10 13:30:49 +0900

    Updates

---
+++ sw.js
@@ -0,0 +1,22 @@
+// Service Worker to disable caching
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', function(event) {
+  // Always fetch from network, never use cache
+  event.respondWith(
+    fetch(event.request, {
+      cache: 'no-cache',
+      headers: {
+        'Cache-Control': 'no-cache, no-store, must-revalidate',
+        'Pragma': 'no-cache',
+        'Expires': '0'
+      }
+    })
+  );
+});
